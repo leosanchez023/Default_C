66ae0b82e734292393b28f6d9b3049bb37601401
leosanchez023 <leonardosanchezsilva500@gmail.com>
2024-05-14 22:45:27 -0300

    Default_C#

---
+++ Default_C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AplicativoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo");
+        }
+    }
+}
